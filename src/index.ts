7cae4daf7aadbb50ddd93f3a50f7ad7d89838226
Chengzhu Duan <Chengzhu.Duan@gmail.com>
2020-10-19 15:58:30 -0700

    Set up boilerplate code for Awesome-Tool Bot

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,16 @@
-import { Application } from 'probot' // eslint-disable-line no-unused-vars
+import { Application, Context } from 'probot' // eslint-disable-line no-unused-vars
 
 export = (app: Application) => {
-  app.on('issues.opened', async (context) => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
+  app.on('issues.opened', async (context: Context) => {
+    const issueComment = context.issue({ body: 'Thanks for opening this issue!' });
     await context.github.issues.createComment(issueComment)
   })
+
+  app.on('pull_request.edited', async (context: Context) => {
+    const issueComment = context.issue({ body: 'Thanks for opening this pull request!' });
+    await context.github.issues.createComment(issueComment)
+  })
+
   // For more information on building apps:
   // https://probot.github.io/docs/
 
