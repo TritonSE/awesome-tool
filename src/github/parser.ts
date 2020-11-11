06a4753e6879b780e7df0a4045513e43d27e8906
Arnold Duan <chengzhu.duan@gmail.com>
2020-11-10 18:17:55 -0800

    Code styling and formatting (#17)

--- src/github/parser.ts
+++ src/github/parser.ts
@@ -1,18 +1,17 @@
 import { ParsedResult, PullRequest } from '../types';
 
 const boardIdRegexp = /board\s+id\s*:\s*(?<board_id>[0-9]+)/i;
-const itemIdRegexp = /.*\/.*\/(?<item_id>[0-9]+)\-/i;
-
+const itemIdRegexp = /.*\/.*\/(?<item_id>[0-9]+)-/i;
 
 export const parseResult = (pullRequest: PullRequest): ParsedResult => {
   const board_id = parseBoardId(pullRequest.body);
   const item_id = parseItemId(pullRequest.head.ref);
-  
+
   return {
     board_id: board_id ? parseInt(board_id) : undefined,
     item_id: item_id ? parseInt(item_id) : undefined,
   };
-}
+};
 
 const parseBoardId = (body: string): string | undefined => {
   const match = body.match(boardIdRegexp)?.groups;
@@ -24,4 +23,4 @@ const parseItemId = (branchName: string): string | undefined => {
   const match = branchName.match(itemIdRegexp)?.groups;
 
   return match?.item_id;
-}
+};
